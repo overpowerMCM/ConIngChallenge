b7fe8ae1144fb8217e626b01fc05bb4fd6cdebc3
poberauer <pablo.oberauer@gmail.com>
2020-06-27 00:36:46 -0300

    datatable contruction stub

--- Assets/UI/DataTable/Scripts/DataTable.cs
+++ Assets/UI/DataTable/Scripts/DataTable.cs
@@ -1,14 +1,40 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace UI.DataTable
 {
     public class DataTable : MonoBehaviour
     {
         TableModel _model;
-        public TableModel Model { get=> _model;
+        public TableModel Model
+        {
+            get => _model;
             set
             {
                 _model = value;
+                SetDirty();
+            }
+        }
+
+        private void SetDirty()
+        {
+            Debug.Log("Rebuild Header");
+            string strHeaders = "";
+            foreach (var e in Model.Headers)
+            {
+                strHeaders += e.Title+" ";
+            }
+            Debug.Log(strHeaders);
+
+            foreach ( var r in Model.Rows )
+            {
+                string strRow = "";
+                foreach (var o in r)
+                {
+                    strRow += o.ToString() + " ";
+                }
+                Debug.Log(strRow);
             }
         }
     }
